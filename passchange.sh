31dcede7f8f1d172b17a1886271ffb83e1616fb0
rootsin <rootsin@users.noreply.github.com>
2015-03-01 15:33:46 +0530

    Change password of all cpanel accounts

---
+++ passchange.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+str0="$$"
+for i in `more list.txt `
+do
+POS=2
+LEN=16 # Make password 8 characters.
+str1=$( echo "$str0" | md5sum | md5sum )
+randstring="${str1:$POS:$LEN}"
+let str0=str0+5
+echo "$i $randstring" >> newpass.txt
+/scripts/chpass $i $randstring && /scripts/mysqlpasswd $i $randstring
+done
