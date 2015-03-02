f4836e6ed37cde22099c75d7e7d98a32c8326928
rootsin <rootsin@users.noreply.github.com>
2015-03-02 18:00:53 +0530

    Change password of all cpanel accounts

--- passchange.sh
+++ passchange.sh
@@ -3,7 +3,7 @@ str0="$$"
 for i in `more list.txt `
 do
 POS=2
-LEN=16 # Make password 8 characters.
+LEN=16 # Make password 16 characters.
 str1=$( echo "$str0" | md5sum | md5sum )
 randstring="${str1:$POS:$LEN}"
 let str0=str0+5
